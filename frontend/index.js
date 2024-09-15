e76c9a5da3d18fa07c2db17165c4dfc674013f05
Matt Glenn <highopenended@gmail.com>
2024-09-15 10:50:03 -0400

    Completed guided project

--- frontend/index.js
+++ frontend/index.js
@@ -3,10 +3,14 @@ import './styles/styles.css'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/App'
+import { Provider } from 'react-redux'
+import { store } from './State/store'
 
 const domNode = document.getElementById('root')
 const root = createRoot(domNode)
 
 root.render(
-  <App />
+  <Provider store = {store}>
+    <App />
+  </Provider>
 )
